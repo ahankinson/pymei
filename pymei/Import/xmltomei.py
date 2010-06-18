dd6eee2a2c6f32d7a6debe499955afaf2710bc3f
Andrew Hankinson <ahankins@Copland-2.local>
2010-06-17 23:16:58 -0400

    modifications to deal with inline text.

--- pymei/Import/xmltomei.py
+++ pymei/Import/xmltomei.py
@@ -1,6 +1,7 @@
 #import lxml #GVM
 from lxml import etree #AH
 from lxml import objectify #AH
+import type
 
 from pymei.Components import MeiDocument, MeiElement
 from pymei.Components import Modules as mod
@@ -22,16 +23,28 @@ def _xml_to_mei(el):
     ns = ns_tag[0].strip('{')
     objname = "{0}_".format(tagname)
     obj = getattr(mod, objname)(namespace=ns)
+    
+    if el.text.strip() != '':
+        # if we strip the blanklines and line endings and do not end up with an
+        # empty string, we have inline text that we need to deal with.
+        # For now, we will strip out *any* child tags to set the text value. We'll 
+        # still store them as children tags elements, but they will be taken 
+        # out of context. It's the best we can do for now.
+        tx = ""
+        for n in el.itertext():
+            tx += " ".join(n.split())
+            tx += " "
+        obj.setvalue(tx)
+        
     if el.items():
         attrs = dict(el.items())
         obj.setattributes(attrs)
-        
+    
     c = list(el)
     if c is True:
         m = map(_xml_to_mei, c)
         obj.addchildren(m)
-    else:
-        obj.setvalue(el.text)
+        
     return obj
 
     
