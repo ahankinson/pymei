62c89fdc13949f6e11acb1687f30345d0d36d0e3
Andrew Hankinson <andrew.hankinson@gmail.com>
2011-01-21 16:59:02 -0500

    added mordent methods

--- pymei/Components/Modules/cmnOrnaments.py
+++ pymei/Components/Modules/cmnOrnaments.py
@@ -6,6 +6,48 @@ class mordent_(MeiElement):
         MeiElement.__init__(self, name=u"mordent", value=value, parent=parent)
         if attrs:
             self.attributes = attrs
+            
+    @property
+    def tstamp(self):
+        if self.has_attribute('tstamp'):
+            return self.attribute_by_name('tstamp').value
+        else:
+            return None
+    @tstamp.setter
+    def tstamp(self, value):
+        self.attributes = {'tstamp': value}
+        
+    @property
+    def place(self):
+        if self.has_attribute('place'):
+            return self.attribute_by_name('place').value
+        else:
+            return None
+    @place.setter
+    def place(self, value):
+        self.attributes = {'place': value}
+    
+    @property
+    def form(self):
+        if self.has_attribute('form'):
+            return self.attribute_by_name('form').value
+        else:
+            return None
+    @form.setter
+    def form(self, value):
+        self.attributes = {'form': value}
+    
+    @property
+    def staff(self):
+        if self.has_attribute('staff'):
+            return self.attribute_by_name('staff').value
+        else:
+            return None
+    @staff.setter
+    def staff(self, value):
+        self.attributes = {'staff': value}
+    
+    
 
 class trill_(MeiElement):
     def __init__(self, value=None, parent=None, **attrs):
