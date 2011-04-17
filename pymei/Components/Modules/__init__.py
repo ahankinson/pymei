834815e8ff33528e8ccf7ed4c2115795a7b2a6a6
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2011-04-16 20:24:25 -0400

    fix solesmes importing

--- pymei/Components/Modules/__init__.py
+++ pymei/Components/Modules/__init__.py
@@ -1,5 +1,6 @@
 __all__ = ['cmn', 'cmnOrnaments', 'corpus', 'critapp', 'edittrans', 'facsimile', 'figtable', 'harmony', 'header',
-            'linkalign', 'lyrics', 'mensural', 'midi', 'namesdates', 'neumes', 'ptrref', 'shared', 'text', 'usersymbols', 'ax_layout']
+            'linkalign', 'lyrics', 'mensural', 'midi', 'namesdates', 'neumes', 'ptrref', 'shared', 'text', 'usersymbols', 'ax_layout',
+            'solesmes']
             
 from pymei.Components.Modules.cmn import *
 from pymei.Components.Modules.cmnOrnaments import *
@@ -15,7 +16,8 @@ from pymei.Components.Modules.lyrics import *
 from pymei.Components.Modules.mensural import *
 from pymei.Components.Modules.midi import *
 from pymei.Components.Modules.namesdates import *
-from pymei.Components.Modules.neumes import *
+# from pymei.Components.Modules.neumes import *
+from pymei.Components.Modules.solesmes import *
 from pymei.Components.Modules.ptrref import *
 from pymei.Components.Modules.shared import *
 from pymei.Components.Modules.text import *
