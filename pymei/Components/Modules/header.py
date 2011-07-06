d48b464c6d029e97517775644cf63c01262634d6
Jamie Klassen <christopher.westleyklassen@mail.mcgill.ca>
2011-07-06 14:02:30 -0400

    changed meihead class

--- pymei/Components/Modules/header.py
+++ pymei/Components/Modules/header.py
@@ -175,9 +175,9 @@ class langusage_(MeiElement):
         if attrs:
             self.attributes = attrs
 
-class meihead_(MeiElement):
+class meiHead_(MeiElement):
     def __init__(self, value=None, parent=None, **attrs):
-        MeiElement.__init__(self, name=u"meihead", value=value, parent=parent)
+        MeiElement.__init__(self, name=u"meiHead", value=value, parent=parent)
         if attrs:
             self.attributes = attrs
 
