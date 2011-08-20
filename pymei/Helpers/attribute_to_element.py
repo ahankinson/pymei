35d759a36268d331b09b36af6fc3ce74ea45fb7d
Raffaele <rviglianti@rviglianti-Inspiron-N5010.(none)>
2011-08-20 15:12:52 +0100

    Moved tstamp function; completed switch_tie()

--- pymei/Helpers/attribute_to_element.py
+++ pymei/Helpers/attribute_to_element.py
@@ -1,73 +1,8 @@
 from pymei.Components import Modules as mod
-from pymei.Helpers import flatten
+from pymei.Helpers import time, generate_mei_id
 
-def id_to_tstamp(event, doc, base=None): 
-	''' Returns the tstamp of one event. 
-	NB: the event needs to be in a layer
-              the layer is supposed to start at beginning of measure
-	'''
-	
-	def add_dot(total, dur):
-		'''given a total number of dots and a dotted event's duration, calculates the relative duration'''
-		rel_dur = 0.0000
-		dur = float(dur)
-		while total != 1:
-			rel_dur += 1 / (dur * 2) 
-			dur = dur*2
-			total -= 1
-		return rel_dur
-		
-	context = event.ancestor_by_name('layer')
-	#if base is not provided, get it from scoredef
-	if base==None:
-		flattened_elements = flatten(doc.root)
-			
-		scoredef = -1
-		for e in flattened_elements:
-			if e.name == 'scoreDef':
-				scoredef = e
-			if e.id == event.id:
-				break
-	
-		if scoredef != -1:
-			base = float(scoredef.attribute_by_name('meter.unit').value)
-			
-	#get all events in context a2120(note + rest. Other events that should be considered?)
-	notes = context.descendants_by_name('note') 
-	rests = context.descendants_by_name('rest')
-	events = []
-	
-	if notes:
-		events += notes
-	if rests:
-		events += rests
-	
-	durations = 0.0000
-	
-	for ev in events:
-		if ev.id == event.id:
-			break
-		#get relative duration of current event
-		#dur = float(event.attribute_by_name('dur').value)
-		
-		#add entry to dictionary
-		durations += 1 /  float(event.attribute_by_name('dur').value)
-		
-		#add dots if present		
-		if event.attribute_by_name('dots') != None:
-			total = float(event.attribute_by_name('dots').value)
-			dur = float(event.attribute_by_name('dur').value)
-			durations += add_dot(total, dur)
-			
-		elif event.descendants_by_name('dot') != None:
-			total = float(len(event.descendants_by_name('dot')))
-			dur = float(event.attribute_by_name('dur').value)
-			durations += add_dot(total, dur)
-			
-	tstamp = (durations / ( 1 / base)) + 1
-	return tstamp
 
-def switch_tie(meifile, tstamp = False ):
+def switch_tie(meifile, tstamp = False, keep_id = False):
 	''' Changes all ties expressed with attributes into elements. 
 	If tstamp is set, it will attempt to generate tstamps instead of startid/endid pairs.
 	
@@ -84,8 +19,19 @@ def switch_tie(meifile, tstamp = False ):
 			if n.tie=='i' or n.tie=='m': #one tie element for each tie!
 				#get ancestor measure
 				measure = n.ancestor_by_name('measure')
-				#create tie and add @startid
-				tie = mod.tie_(startid=n.id)
+				#create a tie element
+				tie = mod.tie_()
+				
+				#determine attributes according to args
+				atts = {'xml:id':generate_mei_id()}
+				if tstamp:
+					atts['tstamp'] =  time.id_to_tstamp(n)
+				if keep_id or (not keep_id and not tstamp):
+					atts['startid'] = n.id
+					
+				#add attributes to tie
+				tie.attributes = atts
+				
 				#add tie to measure
 				measure.add_child(tie)
 		#remove tie attribute
